ef70b0811578d394eeabef34d3545b81fc84498e
Jesse Walsh <845836268@ken20-202-07.local>
2017-10-26 12:57:20 +1300

    wavs-added

---
+++ js/app.js
@@ -0,0 +1,59 @@
+// LISA
+
+
+
+
+
+
+
+
+
+
+
+
+// JESSE
+//Click Function Initialisation
+function init(){
+			var playButton = document.getElementByClassName('#');
+			playButton.addEventListener('click', playAudioA, false);
+
+			var playButton = document.getElementByClassName('#');
+			playButton.addEventListener('click', playAudioB, false);
+			
+			var playButton = document.getElementByClassName('#');
+			playButton.addEventListener('click', playAudioC, false);
+
+			var playButton = document.getElementByClassName('#');
+			playButton.addEventListener('click', playAudioD, false);
+
+			var playButton = document.getElementByClassName('#');
+			playButton.addEventListener('click', playAudioE, false);
+			
+			var playButton = document.getElementByClassName('#');
+			playButton.addEventListener('click', playAudioF, false);
+
+			var playButton = document.getElementByClassName('#');
+			playButton.addEventListener('click', playAudioG, false);
+
+			var playButton = document.getElementByClassName('#');
+			playButton.addEventListener('click', playAudioH, false);
+			
+			var playButton = document.getElementByClassName('#');
+			playButton.addEventListener('click', playAudioI, false);
+
+			var playButton = document.getElementByClassName('#');
+			playButton.addEventListener('click', playAudioJ, false);
+			console.log('init working');
+		}//init end ***
+
+
+// CAM
+
+
+
+
+
+
+
+
+// FORREST
